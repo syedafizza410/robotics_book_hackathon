89a4fbd797a9fbc779174fb0a0d2fe11b5dd6262
syeda ummefizza <162727504+syedafizza410@users.noreply.github.com>
2025-12-07 22:00:33 +0500

    add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
