32be88600a727956629e3c0b59f8bfa80767fefa
syeda ummefizza <162727504+syedafizza410@users.noreply.github.com>
2025-12-09 10:39:17 +0500

    add

--- src/components/readbutton.js
+++ src/components/readbutton.js
@@ -32,7 +32,7 @@ export default function ReadChapterButton() {
 
     setIsLoading(true);
     try {
-      const res = await fetch("http://127.0.0.1:5000/tts", {
+      const res = await fetch("https://robotics-book-hackathon-tts.vercel.app/tts", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text }),
