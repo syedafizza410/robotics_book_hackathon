3cd2d3e524949d0a717d9f567ab12fa337fd6cfd
syeda ummefizza <162727504+syedafizza410@users.noreply.github.com>
2025-12-12 16:46:41 +0500

    add

--- src/components/auth/login.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import LoginPage from "../../pages/login";
-
-export default function Login() {
-  return <LoginPage />;
-}
--- src/components/auth/signup.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import SignupPage from "../../pages/signup";
-
-export default function Signup() {
-  return <SignupPage />;
-}
